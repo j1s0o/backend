a9fb9ff32655735d2f2cf4f15d50e4a207df8e6f
j1s0o <20521902@gm.uit.edu.vn>
2022-11-04 19:25:41 +0700

    i need font end

--- typescript/13.Ejs add phongen/router/apiRouter.ts
+++ typescript/13.Ejs add phongen/router/apiRouter.ts
@@ -6,12 +6,15 @@ const apiRouter:express.Router = express.Router()
 
 
 apiRouter.get('/' , (req:express.Request, res:express.Response) =>{
-    res.status(200).render(path.join('html/main' , 'main'))
+    res.status(200).render(path.join('html/partials' , 'main'))
 })
 
 apiRouter.get('/login' , (req:express.Request, res:express.Response) =>{
     res.status(200).render(path.join('html/user' , 'login'))
 })
 
+apiRouter.get('/register' , (req:express.Request, res:express.Response) =>{
+    res.status(200).render(path.join('html/user' , 'register'))
+})
 
 export default apiRouter
---
+++ typescript/13.Ejs add phongen/views/script/login.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('Login-Form');
+form.addEventListener('submit', LoginUser)
+// send data as JSON
+async function LoginUser(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const result = await fetch('/user/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        }),
+    }).then((res) => res.json())
+    if (result.success === true) {
+        window.location.href = "/";
+    }
+}
